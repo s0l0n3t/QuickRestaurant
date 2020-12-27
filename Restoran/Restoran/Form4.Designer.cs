209fc127aba803478ecaa66129acb646a6b3bbcb
Furkan Tokgöz <furkantokgz@gmail.com>
2020-12-28 00:36:47 +0300

    Git öğrenme aşaması olduğu için az önce ekleyemediğim dosyalarımı ekliyorum.

---
+++ Restoran/Restoran/Form4.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Restoran
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(172, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 195);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Chocolate;
+            this.label1.Location = new System.Drawing.Point(82, 533);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(88, 270);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(204, 244);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(299, 270);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 244);
+            this.listBox2.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(500, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Chocolate;
+            this.label2.Location = new System.Drawing.Point(8, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 55);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 603);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form4";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Restoran/Restoran/Form4.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restoran
+{
+    public partial class Form4 : Form
+    {
+        public int[] dizisepet = new int[12];
+        public string MasaNO_;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+        string SiparisOzet;
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            label1.Text = dizisepet[0].ToString();
+            double fiyat;
+            double toplamfiyat = 0;
+            if (dizisepet[0] > 0)
+            {
+                SiparisOzet = dizisepet[0].ToString() + " adet Et Döner";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 25 * dizisepet[0];
+                toplamfiyat = fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+            }
+            if (dizisepet[1] > 0)
+            {
+                SiparisOzet = dizisepet[1].ToString() + " adet Et Döner Ekmek";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 20 * dizisepet[1];
+                toplamfiyat = toplamfiyat + fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+            }
+            if (dizisepet[2] > 0)
+            {
+                SiparisOzet = dizisepet[2].ToString() + " adet Tombik Et Döner";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 22 * dizisepet[2];
+                toplamfiyat = toplamfiyat + fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+            }
+            if (dizisepet[3] > 0)
+            {
+                SiparisOzet = dizisepet[3].ToString() + " adet Peynirli Tost";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 11 * dizisepet[3];
+                toplamfiyat = toplamfiyat + fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+            }
+            if (dizisepet[4] > 0)
+            {
+                SiparisOzet = dizisepet[4].ToString() + " adet Porsiyon Döner";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 27 * dizisepet[4];
+                toplamfiyat = toplamfiyat + fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+            }
+            if (dizisepet[5] > 0)
+            {
+                SiparisOzet = dizisepet[5].ToString() + " adet Tavuk Döner";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 17 * dizisepet[5];
+                toplamfiyat = toplamfiyat + fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+            }
+            if (dizisepet[6] > 0)
+            {
+                SiparisOzet = dizisepet[6].ToString() + " adet Sucuklu Tost";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 17 * dizisepet[6];
+                toplamfiyat = toplamfiyat + fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+            }
+            if (dizisepet[7] > 0)
+            {
+                SiparisOzet = dizisepet[7].ToString() + " adet Kaşarlı Tost";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 10 * dizisepet[7];
+                toplamfiyat = toplamfiyat + fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+
+            }
+            if (dizisepet[8] > 0)
+            {
+                SiparisOzet = dizisepet[8].ToString() + " adet Sucuklu Kaşarlı";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 20 * dizisepet[8];
+                toplamfiyat = toplamfiyat + fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+            }
+            if (dizisepet[9] > 0)
+            {
+                SiparisOzet = dizisepet[9].ToString() + " adet Ayran";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 7.99 * dizisepet[9];
+                toplamfiyat = toplamfiyat + fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+            }
+            if (dizisepet[10] > 0)
+            {
+                SiparisOzet = dizisepet[10].ToString() + " adet Portakal Suyu";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 7.99 * dizisepet[10];
+                toplamfiyat = toplamfiyat + fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+            }
+            if (dizisepet[11] > 0)
+            {
+                SiparisOzet = dizisepet[11].ToString() + " adet Limonata";
+                listBox1.Items.Add(SiparisOzet);
+                fiyat = 7.99 * dizisepet[11];
+                toplamfiyat = toplamfiyat + fiyat;
+                SiparisOzet = fiyat.ToString() + " ₺";
+                listBox2.Items.Add(SiparisOzet);
+            }
+            label1.Text = "Toplam : " + toplamfiyat.ToString();
+            label2.Text = MasaNO_;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
